43ddf06b84ec9cd53f3bbdbf5f952bcadf3b5da3
zyratechx <zyratechx@gmail.com>
2026-05-02 15:50:00 +0200

    Delete installation-packages.bat

--- installation-packages.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-choice /c YN /d "Do you have NODEJS installed ?"
-if "%errorlevel%" == "2" (winget install nodejs)
-npm install sharp
-npm install argparse
-start https://fonts.google.com/specimen/Russo+One
